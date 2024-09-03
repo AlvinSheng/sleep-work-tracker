4c5a638c5139a64c002af3f396a59cf25e243a01
AlvinSheng <asheng1067@gmail.com>
2024-09-03 11:57:27 -0500

    updating data, and R packages

--- R/scratch.R
+++ R/scratch.R
@@ -5,10 +5,10 @@ sw_dat <- read_delim(here("alvin_sleep_work_data.txt"), delim = " ", col_types =
 
 sw_dat$date <- as.Date(sw_dat$date, "%m/%d/%y")
 
-# TODO: don't omit, but account for it
+
 # for removing the sleep sessions that cross midnight
 cross_midnight <- rep(F, length = nrow(sw_dat))
-# TODO: modify
+
 # average the "#,#" strings in sleep_quality and ease_of_working
 for (i in 1:nrow(sw_dat)) {
   sw_dat$sleep_quality[i] <- mean(component_nums(sw_dat$sleep_quality[i]))
@@ -18,7 +18,7 @@ for (i in 1:nrow(sw_dat)) {
     cross_midnight[i] <- T
   }
 }
-# TODO: figure out why sleep_quality and ease_of_working automatically turns into character
+
 sw_dat$sleep_quality <- as.numeric(sw_dat$sleep_quality)
 sw_dat$ease_of_working <- as.numeric(sw_dat$ease_of_working)
 
